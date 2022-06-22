43e65314fc9ef0bd26b8037a7533f5e7919fd910
Paulabeatriz09 <paula.beatrizmic@hotmail.com>
2022-06-22 18:23:12 -0300

    first commit

---
+++ alocacao.c
@@ -0,0 +1,23 @@
+
+
+//Alocacao dinamica desafio
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+
+//criando um ponteiro para o vetor
+    int var A=22;
+    int *p_var A;
+    pt_var A =&varA;
+
+
+    int*ptr;
+    ptr=(int*)(malloc(22*sizeof(int)));
+    ptr=(int*)(realloc(ptr,22*sizeof(int)));
+    free():void free(void*ptr);
+    
+
+}
